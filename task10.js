3cbc6ff355ec12f33a52013086bd9c8a599a1b13
safonov-work <safonov.itrum@gmail.com>
2023-10-11 14:07:49 +0300

    second solution task10

--- task10.js
+++ task10.js
@@ -22,6 +22,35 @@ const search2 = (nums, target) =>
   nums.includes(target) ? nums.indexOf(target) : -1
 
 console.log(search2(nums, target))
+
+// solution 3
+
+const search3 = (nums, target) => {
+  let left = 0,
+    right = nums.length - 1
+  while (left < right) {
+    const mid = left + Math.floor((right - left) / 2)
+    if (nums[mid] === target) return mid
+
+    if (nums[mid] < nums[right]) {
+      if (target > nums[mid] && target <= nums[right]) {
+        left = mid + 1
+      } else {
+        right = mid
+      }
+    } else {
+      if (target > nums[mid] || target < nums[left]) {
+        left = mid + 1
+      } else {
+        right = mid
+      }
+    }
+  }
+  return left
+}
+
+console.log(search3(nums, target))
+
 // Существует целочисленный массив nums, отсортированный по возрастанию (с разными значениями).
 
 // Перед передачей в вашу функцию numsон , возможно, поворачивается по неизвестному сводному индексу k( 1 <= k < nums.length), так что результирующий массив имеет [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]( индекс 0 ). Например, [0,1,2,4,5,6,7]его можно повернуть по сводному индексу 3и превратить в [4,5,6,7,0,1,2].
