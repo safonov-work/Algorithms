7a193910c0d3f2e9fc426a11d69a896af51477ea
safonov-work <safonov.itrum@gmail.com>
2023-10-13 10:10:06 +0300

    some changes

--- task3.js
+++ task3.js
@@ -1,16 +1,6 @@
 // solution 1;
-const climbStairs = (n) => {
-    let stepsArr = new Array(n);
-    stepsArr[1] = 1;
-    stepsArr[2] = 2;
+const climbStairs = (n) => {}
 
-    for(let i = 3; i <= n; i++) {
-        stepsArr[i] = stepsArr[i-1] + stepsArr[i - 2];
-    }
-    return stepsArr[n]
-}
-
-console.log(climbStairs(5));
+console.log(climbStairs(5))
 
 // solution 2;
-
