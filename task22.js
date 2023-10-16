9944b9ea07dbc35e56eee5d68ee7d920c807df9f
safonov-work <safonov.itrum@gmail.com>
2023-10-16 14:40:27 +0300

    second solution feature/task22

--- task22.js
+++ task22.js
@@ -21,3 +21,24 @@ console.log(productExceptSelf(nums))
 //Произведение любого префикса или суффикса numsгарантированно вписывается в 32-битное целое число.
 
 //Вы должны написать алгоритм, работающий за  O(n) время и не использующий операцию деления.
+
+// solution 2;
+
+var productExceptSelf2 = function (nums) {
+  let n = nums.length
+  let result = Array(n).fill(1)
+  let productFromStart = 1
+  let productFromEnd = 1
+
+  for (let i = nums.length - 1; i >= 0; i--) {
+    result[i] *= productFromEnd
+    productFromEnd *= nums[i]
+  }
+
+  for (let j = 0; j < nums.length; j++) {
+    result[j] *= productFromStart
+    productFromStart *= nums[j]
+  }
+
+  return result
+}
