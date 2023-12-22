119b37d9effbf58429f9ff17fe6747c044024ffb
safonov-work <safonov.itrum@gmail.com>
2023-12-22 09:18:46 +0300

    first sol task84

---
+++ task84.js
@@ -0,0 +1,12 @@
+const expect = (val) => {
+  return {
+    toBe: (val2) => {
+      if (val === val2) return true;
+      else throw new Error('Not Equal');
+    },
+    notToBe: (val2) => {
+      if (val !== val2) return true;
+      else throw new Error('Equal');
+    },
+  };
+};
